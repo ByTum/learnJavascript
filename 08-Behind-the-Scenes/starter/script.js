6008bd323cdc2d973963c442652658ba3af7877a
Naruephon <tum-ok@msn.com>
2022-09-02 11:49:59 +0700

    start09

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -161,7 +161,7 @@ addExpr(2, 5);
 // };
 // addArrow(2, 5, 8, 1);
 */
-
+/*
 let age = 30;
 let oldAge = age;
 age = 31;
@@ -177,3 +177,40 @@ const friend = me;
 friend.age = 27;
 console.log('Friend:', friend);
 console.log('me:', me);
+*/
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+// console.log('lastName', lastName);
+// console.log('oldLastName', oldLastName);
+
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+// console.log('Before marriage', jessica);
+// console.log('After marriage', marriedJessica);
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage', jessica2);
+console.log('After marriage', jessicaCopy);
