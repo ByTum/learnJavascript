9abc3286279f54c1fb77a132506cc257a828b2cc
Naruephon <tum-ok@msn.com>
2022-09-13 19:16:21 +0700

    challenge

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -53,7 +53,69 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+// Coding Challenge 2
+// 1.
+for (const [index, name] of game.scored.entries()) {
+  console.log(`Goal ${index + 1} : ${name}`);
+}
 
+// 2.
+const odds = Object.values(game.odds);
+let averages = 0;
+for (const odd of odds) averages += odd;
+averages /= odds.length;
+
+console.log(averages);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  console.log(team);
+  const teamStr = team === 'x' ? 'draw' : `Victory ${game}`;
+  console.log(game);
+  //console.log(teamStr);
+}
+
+// console.log(odds);
 /*
 // Looping Objects: Object Keys, Values, and Entries
 // Property Name
@@ -135,47 +197,9 @@ for (const [i, el] of menu.entries()) {
 // console.log(...menu.entries());
 /*
 // Coding challenge
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+*/
 
+/*
 // 1.
 const [players1, players2] = game.players;
 // console.log(players1, players2);
