685e5f195e6a6770cd22c5fccf53d8efe10f7507
นฤพนธ์ โชควินิจ <naruephon.c@rtarf.mi.th>
2022-11-08 16:22:23 +0700

    learn

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,6 +54,47 @@ const restaurant = {
   },
 };
 
+// Coding Challenge #3
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔀 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔀 Substitution'],
+  [64, '🟨 Yellow card'],
+  [69, '🟥 Red card'],
+  [70, '🔀 Substitution'],
+  [72, '🔀 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🟨 Yellow card'],
+]);
+
+// 1.
+// const events = new Map(gameEvents);
+const events = new Set(gameEvents.values());
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [key, value] of gameEvents) {
+  const half = key <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${key}: ${value}`);
+}
+// Maps: Iteration การทวน Map
+/*
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
   [1, 'C'],
@@ -91,6 +132,8 @@ console.log([...question]);
 // console.log([...question.entries()]);
 console.log([...question.keys()]);
 console.log([...question.values()]);
+
+*/
 
 // Maps : Fundamentals
 /*
